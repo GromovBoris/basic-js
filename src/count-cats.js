68cf8d8c1cdf053841ac5cb514bc5492e2fbaa05
GromovBoris <drunkgonzo@gmail.com>
2022-10-16 17:07:46 +0300

    test count cats done

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,14 +4,15 @@ function countCats(matrix) {
   let counter = 0;
   for (i = 0; i < matrix.length; i++) {
     for (y = 0; y < matrix[i].length; y++) {
-      // console.log(matrix[y][i]);
-      if (matrix[y][i] == "^^") {
+      // console.log(matrix[i][y]);
+      if (matrix[i][y] === "^^") {
         counter++;
       }
     }
   }
   return counter;
 }
+
 module.exports = {
   countCats,
 };
