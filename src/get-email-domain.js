6e5bb98b1a8b3abb89b09e6cfdc088a52123960e
GromovBoris <drunkgonzo@gmail.com>
2022-10-16 15:09:09 +0300

    test get-email-domain done

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,13 +1,13 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
-function getEmailDomain(mail) {
+function getEmailDomain(email) {
   let flag;
   let domain = String;
-  for (i = 0; i < mail.length; i++) {
-    if (mail[i] == "@") {
+  for (i = 0; i < email.length; i++) {
+    if (email[i] === "@") {
       flag = i + 1;
     }
   }
-  domain = mail.slice(flag, mail.length);
+  domain = email.slice(flag, email.length);
   return domain;
 }
