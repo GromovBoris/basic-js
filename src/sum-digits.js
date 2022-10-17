de682ea7a95ef88231e722d75224e5bfe73a740c
GromovBoris <drunkgonzo@gmail.com>
2022-10-17 12:22:26 +0300

    test sum dogot 1.1

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,17 +1,6 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
 function getSumOfDigits(n) {
-  // let nArr = n.toString().split("").map(Number);
-  // let sum = 0;
-  // for (i = 0; i < nArr.length; i++) {
-  //   sum += nArr[i];
-  // }
-  // answer = sum;
-  // if (answer <= 9) {
-  //   console.log(answer);
-  // } else {
-  //   return answer;
-  // }
   n = n.toString().split("");
   let answer = 0;
   if (n.length != 1) {
