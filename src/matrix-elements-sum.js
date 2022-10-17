85f9389c5b6bfe32b5cce4d4eb7d3c7ba85b00de
GromovBoris <drunkgonzo@gmail.com>
2022-10-17 12:40:56 +0300

    test matrix elements sum done

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,18 +1,23 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
 function getMatrixElementsSum(matrix) {
-  let sum = 0;
+  let answer = 0;
   for (i = 0; i < matrix.length; i++) {
-    for (y = 0; y < matrix[i].length; y++) {
+    for (y = 0; y < matrix[0].length; y++) {
       if (matrix[i][y] == 0 && i + 1 < matrix.length) {
         matrix[i + 1][y] = 0;
       }
-      sum += matrix[i][y];
+      answer += matrix[i][y];
     }
   }
-  return sum;
-  // return counter;
+  return answer;
 }
+
+// getMatrixElementsSum([
+//   [0, 1, 1, 2],
+//   [0, 5, 0, 0],
+//   [2, 0, 3, 3],
+// ]);
 
 module.exports = {
   getMatrixElementsSum,
